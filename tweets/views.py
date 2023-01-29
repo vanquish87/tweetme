26fc3b4121199fee70b154532526b2ae93c8d526
Jasmeet Singh <mailtojasmeet@gmail.com>
2023-01-29 20:37:03 +0530

    20. Like Button Rendering - Done

--- tweets/views.py
+++ tweets/views.py
@@ -1,17 +1,37 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
-from .models import Tweet
+import random
 
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/home.html")
+    return render(request, "pages/home.html", context={}, status=200)
 
-def tweets_list_view(request, *args, **kwargs):
-    return render(request, "tweets/list.html")
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    qs = Tweet.objects.values('id', 'content')
+    for i in qs:
+        i['likes'] = random.randint(0,50)
+
+    data = {
+        "isUser": False,
+        "response": list(qs)
+    }
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     data = {
         "id": tweet_id,
     }
@@ -20,10 +40,6 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         obj = Tweet.objects.get(id=tweet_id)
         data['content'] = obj.content
     except:
-        data['message'] = "Not Found"
+        data['message'] = "Not found"
         status = 404
-    return JsonResponse(data=data, status=status)
-    
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello tweet No. {kwargs['tweet_id']} </h1>")
-    return render(request, "tweets/list.html")
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
