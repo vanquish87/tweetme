9dc3f887aa4c840629449c5ee517f010c661a01b
Jasmeet Singh <mailtojasmeet@gmail.com>
2023-01-30 19:05:22 +0530

    26. Prevent Form Submit via JavaScript

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zkqhz6lf%5)g3x7k*=rmrnjokvl8ra81yymzct19w=ayj_tpgq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,36 @@
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
+from .forms import TweetForm
+from django.conf import settings
+from django.utils.http import is_safe_url
 
 from .models import Tweet
+
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+
+        form = TweetForm()
+
+    context={
+        "form": form
+        }
+    return render(request, 'components/form.html', context)
 
 
 def tweet_list_view(request, *args, **kwargs):
