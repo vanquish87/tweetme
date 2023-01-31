48f0eb8bb898444bccadd1c194d8230b7fa9d642
Jasmeet Singh <mailtojasmeet@gmail.com>
2023-01-31 11:49:09 +0530

    36. Permissions & Roadmap - Done

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 import random
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
@@ -12,7 +16,7 @@ class Tweet(models.Model):
 
     def __str__(self):
         return self.content
-        
+
     def serialize(self):
         return {
             'id': self.id,
--- tweets/views.py
+++ tweets/views.py
@@ -16,11 +16,20 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
+
+
     form = TweetForm(request.POST or None)
     next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
+        obj.user = request.user
         obj.save()
         if request.is_ajax():
             return JsonResponse( obj.serialize(), status=201)
@@ -28,6 +37,9 @@ def tweet_create_view(request, *args, **kwargs):
             return redirect(next_url)
 
         form = TweetForm()
+
+    if form.errors and request.is_ajax():
+        return JsonResponse(form.errors, status=400)
 
     context={
         "form": form
